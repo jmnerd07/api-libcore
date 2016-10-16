533e802d3b127234088a3a5d7e8972368c065cbe
JM <jmnerd07@gmail.com>
2016-10-17 02:22:21 +0800

    Routes for showing all books and keyword searching of books

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,25 @@
     return $request->user();
 })->middleware('auth:api');
 */
-Route::group(['prefix'=>'/book-service', 'middleware'=>'auth:api'], function() {
+Route::group(['prefix'=>'/book-service', 'middleware'=>'auth:api', 'as'=>'bookService::'], function() {
 	
+	/**
+	 * Show all books
+	 *
+	 * @return Object result
+	 */
+	Route::get('/',['as'=>'listBooks', 'uses'=>'Services\BooksController@index']);
+	
+	/**
+	 * Search books by title, description or ISBN
+	 *
+	 * @param string $keyword
+	 * @return Object result
+	 */
+	Route::post('/search', ['as'=>'searchBooks', 'uses'=>'Services\BooksController@index']);
+
+	/**
+	 * Search books by keywords, publishing date, publisher, author, format or availability
+	 */
+	//Route::post('/advance-search', ['as'=>'advanceSearchBooks', 'uses'=>'Services\BooksController@index']);
 });
